655719fe38e667dfa6be7b37f517485da038d097
Hoang Anh <iamnguyenhoanganh@outlook.com>
2023-03-04 22:53:28 +0700

    first commit

---
+++ public/js/login.js
@@ -0,0 +1,64 @@
+/* eslint-disable */
+// import '@babel/polyfill';
+// const axios = require('axios');
+const loginForm = document.querySelector('.form--login');
+const logOutBtn = document.querySelector('.nav__el--logout');
+
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+// type is 'success' or 'error'
+const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
+
+const login = async (email, password) => {
+  try {
+    console.log(email, password);
+    const res = await axios({
+      method: 'POST',
+      url: 'http://localhost:8000/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+    console.log(res);
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('http://2.40.45.90/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
+
+const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://localhost:8000/api/v1/users/logout'
+    });
+    if ((res.data.status = 'success')) location.reload(true);
+  } catch (err) {
+    console.log(err.response);
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
+if (loginForm)
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('hehe');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
